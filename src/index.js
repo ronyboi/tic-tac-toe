c09aa4fc3eb11776b22fe8af1374587f9c0ebdb4
Rohan Agarwal <arohan2907@gmail.com>
2021-05-20 20:08:53 +0530

    Wording changed.

--- src/index.js
+++ src/index.js
@@ -99,7 +99,7 @@ class Game extends React.Component {
 
     let status;
     if (winner) {
-      status = "Winner: " + winner;
+      status = "Winner Chicken Dinner: " + winner;
     } else {
       status = "Next player: " + (this.state.xIsNext ? "X" : "O");
     }
